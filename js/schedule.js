d69c2961c2c7ab287f06cfa20338af81f9955ffb
Raymond Piller <github.com@80.vertigion.com>
2014-12-20 17:37:52 -0600

    Did some polishing. Things are in a working state.

--- js/schedule.js
+++ js/schedule.js
@@ -1,38 +1,108 @@
-$(document).ready(function() {
-// page is now ready, initialize the calendar...
-	$('#calendar').fullCalendar({
-		loading: function(bool) {
-		  if (bool) 
-		    $('#loading').show();
-		  else 
-		    $('#loading').hide();
-		},
-		header: {
-			left: 'prev,next today',
-			center: 'title',
-			right: 'month,agendaWeek,agendaDay'
-		},
-		height: 'auto',
-		eventSources: [
+// jquery.xdomainajax.js  ------ from padolsey
 
-	        // your event source
-	        {
-	        	url: ('https:' == document.location.protocol ? 'https:' : 'http:') +'//proactivedefense.vertigion.com/schedule.json.php',
-			    cache: true,
-	            type: 'POST',
-	            data: {
-	                custom_param1: 'something',
-	                custom_param2: 'somethingelse'
-	            },
-	            error: function() {
-	                alert('there was an error while fetching events!');
-	            },
-	            // color: 'yellow',   // a non-ajax option
-	            // textColor: 'black' // a non-ajax option
-	        }
+jQuery.ajax = (function(_ajax){
 
-	        // any other sources...
+    var protocol = location.protocol,
+        hostname = location.hostname,
+        exRegex = RegExp(protocol + '//' + hostname),
+        YQL = 'http' + (/^https/.test(protocol)?'s':'') + '://query.yahooapis.com/v1/public/yql?callback=?',
+        query = 'select * from html where url="{URL}" and xpath="*"';
 
-	    ]
-	})
+    function isExternal(url) {
+        return !exRegex.test(url) && /:\/\//.test(url);
+    }
+
+    return function(o) {
+        var url = o.url;
+        if ( /get/i.test(o.type) && !/json/i.test(o.dataType) && isExternal(url) ) {
+            // Manipulate options so that JSONP-x request is made to YQL
+            o.url = YQL;
+            o.dataType = 'json';
+            o.data = {
+                q: query.replace(
+                    '{URL}',
+                    url + (o.data ?
+                        (/\?/.test(url) ? '&' : '?') + jQuery.param(o.data)
+                    : '')
+                ),
+                format: 'xml'
+            };
+
+            // Since it's a JSONP request
+            // complete === success
+            if (!o.success && o.complete) {
+                o.success = o.complete;
+                delete o.complete;
+            }
+
+            o.success = (function(_success){
+                return function(data) {
+
+                    if (_success) {
+                        // Fake XHR callback.
+                        _success.call(this, {
+                            responseText: data.results[0]
+                                // YQL screws with <script>s
+                                // Get rid of them
+                                .replace(/<script[^>]+?\/>|<script(.|\s)*?\/script>/gi, '')
+                        }, 'success');
+                    }
+                };
+            })(o.success);
+        }
+
+        return _ajax.apply(this, arguments);
+    };
+
+})(jQuery.ajax);
+
+$(document).ready(function(text) {
+    // Pull event_list content ...
+$.ajax({
+    url: 'http://proactivedefense.vertigion.com/schedule.json.php?html=1',
+    type: 'GET',
+    success: function(res) {
+        alert(res.responseText);
+        $('#event_list').html(res.responseText);
+    },
+    dataType: 'html'
+});
+
+    $('#calendar').fullCalendar({
+        loading: function(bool) {
+          if (bool) 
+            $('#loading').show();
+          else 
+            $('#loading').hide();
+        },
+        header: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'month,agendaWeek,agendaDay'
+        },
+        height: 'auto',
+        eventSources: [
+
+            // your event source
+            {
+                url: ('https:' == document.location.protocol ? 'https:' : 'http:') +'//proactivedefense.vertigion.com/schedule.json.php',
+                cache: true,
+                type: 'POST',
+                data: {
+                    custom_param1: 'something',
+                    custom_param2: 'somethingelse'
+                },
+                error: function() {
+                    alert('there was an error while fetching events!');
+                },
+                // color: 'yellow',   // a non-ajax option
+                // textColor: 'black' // a non-ajax option
+            }
+        ],
+        eventRender: function(event, element) {
+            element.qtip({
+                content: (event.img != null ? '<img src="'+ event.img +'" alt="Class Image" />' : '') +'<pre>'+ event.description +'</pre>'
+            });
+        }   
+    })
 });
--- schedule.json.php
+++ schedule.json.php
@@ -1,15 +1,37 @@
 <?php
-header('Access-Control-Allow-Origin: http://www.proactivedefense.net');
+$allowed_http_origins   = array(
+                            'http://www.proactivedefense.net',
+							'https://www.proactivedefense.net',
+							'http://fiddle.jshell.net',
+							'https://fiddle.jshell.net',
+                          );
+if (in_array($_SERVER['HTTP_ORIGIN'], $allowed_http_origins)){  
+    @header("Access-Control-Allow-Origin: ". $_SERVER['HTTP_ORIGIN']);
+}
 
 $start = ($_REQUEST['start'] ? date('Y-m-d', strtotime($_REQUEST['start'])) : date('Y-m-d'));
 $end = ($_REQUEST['end'] ? date('Y-m-d', strtotime($_REQUEST['end'])) : date('Y-m-d', strtotime('today + 30 days')));
+
+function className($class) {
+	$className = array();
+	
+	if ($class['IsCanceled']) {
+		$className[] = 'IsCanceled';
+	}
+
+	return join(' ', $className);
+}
+
+function generateHtml($arr_json) {
+	return '<pre>'. print_r($arr_json, true) .'</pre>';
+}
 
 require_once 'mindbody-php-api/MB_API.php'; // FROM: https://github.com/devincrossman/mindbody-php-api.git
 $mb = new MB_API(json_decode(file_get_contents("mindbody-sourceCredentials.json"), true));
 
 $data = $mb->GetClasses(array('StartDateTime'=>$start, 'EndDateTime'=>$end));
 
-if(!empty($data['GetClassesResult']['Classes']['Class'])) {
+if (!empty($data['GetClassesResult']['Classes']['Class'])) {
 	$classes = $mb->makeNumericArray($data['GetClassesResult']['Classes']['Class']);
 	if ($_GET['print_r']) {
 		echo '<pre>';
@@ -18,6 +40,9 @@ if(!empty($data['GetClassesResult']['Classes']['Class'])) {
 	} else {
 		$json = array();
 		foreach($classes as $class) {
+			if ($class['HideCancel'])
+				continue;
+
 			$sDate = date('m/d/Y', strtotime($class['StartDateTime']));
 			$sLoc = $class['Location']['ID'];
 			$sTG = $class['ClassDescription']['Program']['ID'];
@@ -25,19 +50,26 @@ if(!empty($data['GetClassesResult']['Classes']['Class'])) {
 			$sclassid = $class['ClassScheduleID'];
 			$studioid = $class['Location']['SiteID'];
 
-
 			array_push($json, array(
+			    "id" => $class['ClassScheduleID'],
 			    "title" => $class['ClassDescription']['Name'],
 			    "start" => date('Y-m-d H:i:s', strtotime($class['StartDateTime'])),
 			    "end" => date('Y-m-d H:i:s', strtotime($class['EndDateTime'])),
 			    "url" => "https://clients.mindbodyonline.com/ws.asp?sDate={$sDate}&sLoc={$sLoc}&sTG={$sTG}&sType={$sType}&sclassid={$sclassid}&studioid={$studioid}",
-			    "className" => ($class['IsCanceled'] ? "class_canceled" : "")
+			    "className" => ClassName($class),
+			    "img" => $class['ClassDescription']['ImageURL'],
+			    "description" => $class['ClassDescription']['Description']
 			));
 		}
-		echo json_encode($json);
+
+		if ($_GET['html']) {
+			echo generateHtml($json);
+		} else {
+			echo json_encode($json);
+		}
 	}
 } else {
-	if(!empty($data['GetClassesResult']['Message'])) {
+	if (!empty($data['GetClassesResult']['Message'])) {
 		echo $data['GetClassesResult']['Message'];
 	} else {
 		echo "Error getting classes<br />";
