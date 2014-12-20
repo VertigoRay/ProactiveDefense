d20c73c3c526ec273ad42a8d57f6aa4f4287332e
Raymond Piller <github.com@80.vertigion.com>
2014-12-20 17:36:48 -0600

    Added more files for working on foo.

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<title>Schedule .: Proactive Defense</title>
+<link rel='stylesheet' href='//cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.1.1/fullcalendar.min.css' />
+<link rel='stylesheet' href='//cdnjs.cloudflare.com/ajax/libs/qtip2/2.2.1/jquery.qtip.min.css' />
+<!-- <link rel='stylesheet' href='//cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.1.1/fullcalendar.print.css' /> -->
+<script src='//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js'></script>
+<script src='//cdnjs.cloudflare.com/ajax/libs/moment.js/2.8.3/moment.min.js'></script>
+<script src='//cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.1.1/fullcalendar.min.js'></script>
+<script src='//cdnjs.cloudflare.com/ajax/libs/qtip2/2.2.1/jquery.qtip.min.js'></script>
+<link rel='stylesheet' href='/css/schedule.css' />
+<script src='/js/schedule.js'></script>
+</head>
+<body>
+    <div id='calendar_wrapper'>
+        <div id='calendar'></div>
+        <div id='loading'><h1>Loading ...</h1><span class="helper"></span><img alt='loading' src='//proactivedefense.vertigion.com/images/loading.gif' /></div>
+    </div>
+    <div id='event_list'></div>
+</body>
+</html>
