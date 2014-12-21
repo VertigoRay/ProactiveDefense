50b94f99d418f53d470440e76fa63d331ec56277
Raymond Piller <github.com@80.vertigion.com>
2014-12-21 02:39:11 -0600

    Use Bootstrap for Mobile Browsers.

--- js/schedule.js
+++ js/schedule.js
@@ -1,6 +1,26 @@
 $(document).ready(function(text) {
-    if ($.cookie('is_mobile') == 1 && $.cookie('mobile_disable') == 0) {
+    if ($.cookie('is_mobile') == 1 && (!$.cookie('disable_mobile') || $.cookie('disable_mobile') == 0)) {
         // These cookies are set by weebly.com
+        // alert('is_mobile: 1')
+        $('head').append('<script src="//proactivedefense.vertigion.com/prd/js/bootstrapmodal.min.js"></script>');
+        $('head').append('<link rel="stylesheet" href="//proactivedefense.vertigion.com/prd/css/bootstrapmodal.css" />');
+        $('body').append('\
+            <div id="fullCalModal" class="modal fade">\
+                <div class="modal-dialog">\
+                    <div class="modal-content">\
+                        <div class="modal-header">\
+                            <h4 id="modalTitle" class="modal-title"></h4>\
+                            <span id="modalTime"></span>\
+                        </div>\
+                        <div id="modalBody" class="modal-body"></div>\
+                        <div class="modal-footer">\
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>\
+                            <button class="btn btn-primary"><a id="eventUrl" target="_blank">Event Page</a></button>\
+                        </div>\
+                    </div>\
+                </div>\
+            </div>'
+        );
 
         $('#calendar').fullCalendar({
             loading: function(bool) {
@@ -18,30 +38,24 @@ $(document).ready(function(text) {
             eventSources: [{
                     url: ($(location).attr('host') != 'proactivedefense.vertigion.com' ? (('https:' == $(location).attr('protocol') ? 'https:' : 'http:') +'//proactivedefense.vertigion.com/prd/') : ($(location).attr('pathname').match("^/prd") ? '/prd/' : '/dev/' )) +'schedule.json.php',
                     cache: true,
-                    method: 'POST',
+                    type: 'POST',
                     data: {
-                        mobile: 1
+                        mobile: 1,
                     },
                     error: function() {
                         alert('there was an error while fetching events!');
                     },
                 }
             ],
-            eventClick: function(event, jsEvent, view) {
-                element.qtip({
-                    content: {
-                        title: event.title + '<br /><i>'+ $.format.date(event.StartDateTime, "MMM d h:mm p") +' - '+ ($.format.date(event.StartDateTime, "MMddyyyy") == $.format.date(event.EndDateTime, "MMddyyyy") ? $.format.date(event.EndDateTime, "h:mm p") : $.format.date(event.EndDateTime, "MMM d h:mm p") ) +'</i>',
-                        text: (event.img != null ? '<img src="'+ event.img +'" alt="Class Image" style="text-align:center" />' : '') + event.description +'<hr /><a href="'+ event.url +'">Register</a>',
-                    },
-                    position: {
-                        my: 'center',
-                        at: 'center',
-                        target: $(window),
-                    },
-                });
-            }   
-        })
-
+            eventClick:  function(event, jsEvent, view) {
+                $('#modalTitle').html(event.title);
+                $('#modalTime').html('<i>'+ $.format.date(event.StartDateTime, "MMM d h:mm p") +' - '+ ($.format.date(event.StartDateTime, "MMddyyyy") == $.format.date(event.EndDateTime, "MMddyyyy") ? $.format.date(event.EndDateTime, "h:mm p") : $.format.date(event.EndDateTime, "MMM d h:mm p") ) +'</i>');
+                $('#modalBody').html((event.img != null ? '<img src="'+ event.img +'" alt="Class Image" style="text-align:center" />' : '') + event.description);
+                $('#eventUrl').attr('href',event.url);
+                $('#fullCalModal').modal();
+                return false;
+            }
+        });
     } else {
         $('#calendar').fullCalendar({
             loading: function(bool) {
@@ -75,6 +89,6 @@ $(document).ready(function(text) {
                     },
                 });
             }   
-        })
+        });
     }
 })
